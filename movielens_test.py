24f17f9a90c194e77a421155fd8019808d987a7b
Dongwoo Kim <arongdari@gmail.com>
2017-04-13 16:22:33 +1000

    Initial commit

---
+++ movielens_test.py
@@ -0,0 +1,59 @@
+import itertools
+import os
+
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import KFold
+
+from smf import SMF
+
+num_user = 943
+num_item = 1682
+
+n_steps = 200000
+batch_sizes = [256, 512, 1024]
+reg_lambdas = [0, 1e-10, 1e-7, 1e-5]
+learning_rates = [0.001, 0.002, 0.01]
+latent_dims = [25, 50]
+
+def read_dataset():
+    M = np.zeros([num_user, num_item])
+    with open('./data/ml-100k/u.data', 'r') as f:
+        for line in f.readlines():
+            tokens = line.split()
+            user_id = int(tokens[0]) - 1  # 0 base index
+            item_id = int(tokens[1]) - 1
+            rating = int(tokens[2])
+            M[user_id, item_id] = rating
+    return M
+
+
+def train_test_validation():
+    M = read_dataset()
+
+    num_rating = np.count_nonzero(M)
+    idx = np.arange(num_rating)
+    np.random.seed(0)
+    np.random.shuffle(idx)
+
+    train_prop = 0.9
+    valid_prop = train_prop * 0.05
+    train_idx = idx[:int((train_prop-valid_prop) * num_rating)]
+    valid_idx = idx[int((train_prop-valid_prop) * num_rating):int(train_prop * num_rating)]
+    test_idx = idx[int(0.9 * num_rating):]
+
+    for learning_rate, batch_size, reg_lambda, latent_dim in itertools.product(learning_rates, batch_sizes, reg_lambdas, latent_dims):
+        result_path = "{0}_{1}_{2}_{3}".format(learning_rate, batch_size, reg_lambda, latent_dim)
+        if not os.path.exists(result_path + "/model.ckpt.index"):
+            with tf.Session() as sess:
+                model = SMF(sess, M, latent_dim, learning_rate=learning_rate, batch_size=batch_size, reg_lambda=reg_lambda)
+                best_mse, best_mae = model.train_test_validation(
+                    M, train_idx=train_idx, test_idx=test_idx, valid_idx=valid_idx, n_steps=n_steps, result_path=result_path)
+
+                print("Best MSE = {0}, best MAE = {1}".format(best_mse, best_mae))
+                with open('result.csv', 'a') as f:
+                    f.write("{0},{1},{2},{3},{4},{5}\n".format(learning_rate, batch_size, reg_lambda, latent_dim, best_mse, best_mae))
+        tf.reset_default_graph()
+
+if __name__ == '__main__':
+    train_test_validation()
---
+++ smf.py
@@ -0,0 +1,123 @@
+import tensorflow as tf
+import numpy as np
+
+def weight_variable(shape, name):
+    initial = tf.truncated_normal(shape, stddev=0.001)
+    return tf.Variable(initial, name=name)
+
+
+def bias_variable(shape, name):
+    b_init = tf.constant_initializer(0.)
+    return tf.get_variable(name, shape, initializer=b_init)
+
+
+class SMF:
+    def __init__(self, sess, M, k, learning_rate=0.001, batch_size=256, reg_lambda=0.01):
+        self.sess = sess
+        self.n, self.m = M.shape
+        self.k = k
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.reg_lambda = tf.constant(reg_lambda, dtype=tf.float32)
+        self.build_graph()
+
+    def build_graph(self):
+        self.u_idx = tf.placeholder(tf.int32, [None])
+        self.v_idx = tf.placeholder(tf.int32, [None])
+        self.r = tf.placeholder(tf.float32, [None])
+
+        self.U = weight_variable([self.n, self.k], 'U')
+        self.V = weight_variable([self.m, self.k], 'V')
+
+        self.U_embed = tf.nn.embedding_lookup(self.U, self.u_idx)
+        self.V_embed = tf.nn.embedding_lookup(self.V, self.v_idx)
+        self.r_hat = tf.reduce_sum(tf.multiply(self.U_embed, self.V_embed), reduction_indices=1)
+
+        self.MSE = tf.losses.mean_squared_error(self.r, self.r_hat)
+        self.MAE = tf.reduce_mean(tf.abs(tf.subtract(self.r, self.r_hat)))
+        self.l2_loss = tf.add(tf.nn.l2_loss(self.U), tf.multiply(self.reg_lambda, tf.nn.l2_loss(self.V)))
+        self.reg_loss = tf.add(self.MSE, self.l2_loss)
+
+        self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
+        self.train_step_u = self.optimizer.minimize(self.reg_loss, var_list=[self.U])
+        self.train_step_v = self.optimizer.minimize(self.reg_loss, var_list=[self.V])
+
+        tf.summary.scalar("MSE", self.MSE)
+        tf.summary.scalar("MAE", self.MAE)
+        tf.summary.scalar("L2-Loss", self.l2_loss)
+        tf.summary.scalar("Reg-Loss", self.reg_loss)
+
+        # add op for merging summary
+        self.summary_op = tf.summary.merge_all()
+
+        # add Saver ops
+        self.saver = tf.train.Saver()
+
+    def construct_feeddict(self, u_idx, v_idx, M):
+        return {self.u_idx:u_idx, self.v_idx:v_idx, self.r:M[u_idx, v_idx]}
+
+    def train_test_validation(self, M, train_idx, test_idx, valid_idx, n_steps=100000, result_path='result/'):
+        nonzero_u_idx = M.nonzero()[0]
+        nonzero_v_idx = M.nonzero()[1]
+
+        train_size = train_idx.size
+        trainM = np.zeros(M.shape)
+        trainM[nonzero_u_idx[train_idx], nonzero_v_idx[train_idx]] = M[
+            nonzero_u_idx[train_idx], nonzero_v_idx[train_idx]]
+
+        best_val_mse = np.inf
+        best_val_mae = np.inf
+        best_test_mse = 0
+        best_test_mae = 0
+
+        train_writer = tf.summary.FileWriter(result_path + '/train', graph=self.sess.graph)
+        valid_writer = tf.summary.FileWriter(result_path + '/validation', graph=self.sess.graph)
+        test_writer = tf.summary.FileWriter(result_path + '/test', graph=self.sess.graph)
+
+        self.sess.run(tf.global_variables_initializer())
+
+        for step in range(1, n_steps):
+            batch_idx = np.random.randint(train_size, size=self.batch_size)
+            u_idx = nonzero_u_idx[train_idx[batch_idx]]
+            v_idx = nonzero_v_idx[train_idx[batch_idx]]
+            feed_dict = self.construct_feeddict(u_idx, v_idx, trainM)
+
+            self.sess.run(self.train_step_v, feed_dict=feed_dict)
+            _, mse, mae, summary_str = self.sess.run(
+                [self.train_step_u, self.MSE, self.MAE, self.summary_op], feed_dict=feed_dict)
+            train_writer.add_summary(summary_str, step)
+
+            if step % int(n_steps / 100) == 0:
+                valid_u_idx = nonzero_u_idx[valid_idx]
+                valid_v_idx = nonzero_v_idx[valid_idx]
+                feed_dict = self.construct_feeddict(valid_u_idx, valid_v_idx, M)
+                mse_valid, mae_valid, summary_str = self.sess.run(
+                    [self.MSE, self.MAE, self.summary_op], feed_dict=feed_dict)
+
+                valid_writer.add_summary(summary_str, step)
+
+                test_u_idx = nonzero_u_idx[test_idx]
+                test_v_idx = nonzero_v_idx[test_idx]
+                feed_dict = self.construct_feeddict(test_u_idx, test_v_idx, M)
+                mse_test, mae_test, summary_str = self.sess.run(
+                    [self.MSE, self.MAE, self.summary_op], feed_dict=feed_dict)
+
+                test_writer.add_summary(summary_str, step)
+
+                print("Step {0} | Train MSE: {1:3.4f}, MAE: {2:3.4f}".format(
+                    step, mse, mae))
+                print("         | Valid  MSE: {0:3.4f}, MAE: {1:3.4f}".format(
+                    mse_valid, mae_valid))
+                print("         | Test  MSE: {0:3.4f}, MAE: {1:3.4f}".format(
+                    mse_test, mae_test))
+
+                if best_val_mse > mse_valid:
+                    best_val_mse = mse_valid
+                    best_test_mse = mse_test
+
+                if best_val_mae > mae_valid:
+                    best_val_mae = mae_valid
+                    best_test_mae = mae_test
+
+        self.saver.save(self.sess, result_path + "/model.ckpt")
+        return best_test_mse, best_test_mae
